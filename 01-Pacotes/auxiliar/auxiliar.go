db739021c5fd24d8cddafd33a48de1311c28ef19
Humberto Moura <humbertomoura.n@gmail.com>
2021-10-14 16:44:11 -0300

    adição de pasta sobre pacotes

---
+++ 01-Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Com letra inicial maiuscula ele pode ser visto fora do pacote.
+func Escrever() {
+	fmt.Println("Escrevendo da func Auxiliar")
+	escrever2()
+}
---
+++ 01-Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//como a função começa por minuscula, só pode ser encontrada dentro do package auxiliar
+func escrever2() {
+	fmt.Println("Escrevendo da func Auxiliar2")
+}
